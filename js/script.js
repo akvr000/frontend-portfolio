afa39bbe3c73704c2094f8c0c990d4a5636d909f
akvr000 <akvr46921@gmail.com>
2026-05-12 11:14:36 +0530

    improvement in navbar,hamburger icon and theme icon

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ typeEffect();
 // added hamburger menu functionality
 
 const hamburger = document.getElementById("hamburger");
-const nav = document.querySelector("nav");
+const nav = document.querySelector(".inner-navbar");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
