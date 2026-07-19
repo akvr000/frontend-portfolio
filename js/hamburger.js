cc99b87f5d38d54c8a45accc30bd9fb56df21b01
akvr000 <akvr46921@gmail.com>
2026-07-19 17:55:31 +0530

    added navbar and header effect and auto close after clicking link and fixed color issue while switching from light to dark mode

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,15 @@
 const hamburger = document.getElementById("hamburger");
 const nav = document.querySelector(".inner-navbar");
 
-hamburger.addEventListener("click", () => {
-  hamburger.querySelectorAll("span").forEach(span => span.classList.toggle("show"));
-  nav.classList.toggle("active");
+hamburger.addEventListener("click", () =>{
+  hamburger.classList.toggle("show")
+  nav.classList.toggle("active")
+})
+
+// Close menu when a link is clicked
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('show');
+    nav.classList.remove('active');
+  });
 });
--- js/main.js
+++ js/main.js
@@ -15,6 +15,16 @@ const yearSpan = document.getElementById("current-year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
 
+// add navbar scroll effect
+const navbar = document.querySelector("#header");
+window.addEventListener('scroll',() =>{
+    if (window.scrollY>50) {
+        navbar.classList.add('scrolled');
+    }else{
+        navbar.classList.remove('scrolled');
+    }
+});
+
 // reset form on page load
 window.onpageshow = function(event) {
     const form = document.querySelector("form");
